1a20d94ea6d4f16781ad185e37bc2d87add0ddd5
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2016-12-05 15:44:38 +0800

    added migrations

--- database/migrations/2016_12_05_061825_add_applies_to_location_field_on_approval_setup.php
+++ database/migrations/2016_12_05_061825_add_applies_to_location_field_on_approval_setup.php
@@ -15,8 +15,55 @@ class AddAppliesToLocationFieldOnApprovalSetup extends Migration {
         try {
             DB::beginTransaction();
 
-            Schema::table('tblCOM_ApprovalSetup', function ($table) {
-                $table->string('AS_Location', 30)->nullable();
+            Schema::dropIfExists('tblCOM_ModuleApprovalSetupVariables');
+            Schema::dropIfExists('tblCOM_ModuleApprovalSetup');
+
+            Schema::create('tblCOM_ModuleApprovalSetup', function (Blueprint $table) {
+                $table->string("MAS_Id", 50);
+                $table->bigInteger('MAS_FK_M_Module_id')->unsigned();
+                $table->string('MAS_Location', 30)->default('All');
+                $table->string('MAS_FK_AS_Name', 30);
+                $table->string('MAS_HeaderTableName', 40)->nullable();
+                $table->string('MAS_DetailTableName', 40)->nullable();
+
+                $table->string('CreatedBy', 30);
+                $table->timestamp('DateCreated');
+                $table->string('ModifiedBy', 30);
+                $table->timestamp('DateModified');
+
+                $table->primary(['MAS_Id']);
+
+                $table->foreign('MAS_FK_AS_Name')
+                        ->references('AS_Name')
+                        ->on('tblCOM_ApprovalSetup')
+                        ->onDelete('cascade')
+                        ->onUpdate('cascade');
+            });
+
+            Schema::create('tblCOM_ModuleApprovalSetupVariables', function (Blueprint $table) {
+                $table->string('MASV_Id', 50);
+                $table->string('MASV_Variable', 30);
+                $table->string('MASV_SourceType', 30);
+                $table->string('MASV_SourceOrValue');
+
+                $table->string('CreatedBy', 30);
+                $table->timestamp('DateCreated');
+                $table->string('ModifiedBy', 30);
+                $table->timestamp('DateModified');
+
+                $table->primary(['MASV_Id']);
+
+                $table->foreign('MASV_Id')
+                        ->references('MAS_Id')
+                        ->on('tblCOM_ModuleApprovalSetup')
+                        ->onDelete('cascade')
+                        ->onUpdate('cascade');
+
+//                $table->foreign('MASV_Variable')
+//                        ->references('ASV_FK_AS_Name')
+//                        ->on('tblCOM_ApprovalSetupVariables')
+//                        ->onDelete('cascade')
+//                        ->onUpdate('cascade');
             });
 
             DB::commit();
@@ -35,9 +82,8 @@ class AddAppliesToLocationFieldOnApprovalSetup extends Migration {
         try {
             DB::beginTransaction();
 
-            Schema::table('tblCOM_ApprovalSetup', function ($table) {
-                $table->dropColumn('AS_Location');
-            });
+            Schema::dropIfExists('tblCOM_ModuleApprovalSetupVariables');
+            Schema::dropIfExists('tblCOM_ModuleApprovalSetup');
 
             DB::commit();
         } catch (Exception $e) {
