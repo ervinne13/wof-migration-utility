0fc4a4c12112382c2c60fd4b19f6dee6b53a84d5
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-03-14 06:45:37 +0800

    added migrations

---
+++ database/migrations/2017_03_13_223850_add_remaining_qty_in_rqld.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRemainingQtyInRqld extends Migration
+{
+   /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblINV_RQLDetail', function ($table) {
+
+                if (!Schema::hasColumn('tblINV_RQLDetail', 'RQLD_RemainingQty')) {
+                    $table->boolean('RQLD_RemainingQty')->default(false);
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblINV_RQLDetail', function ($table) {
+
+                if (Schema::hasColumn('tblINV_RQLDetail', 'RQLD_RemainingQty')) {
+                    $table->dropColumn('RQLD_RemainingQty');
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+}
