36a178e0c4cb75216dd6648aa69c84307641e87e
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-03-14 14:12:31 +0800

    added migrations

--- database/migrations/2017_03_14_060749_add_remaining_stock_on_hand_on_rqld.php
+++ database/migrations/2017_03_14_060749_add_remaining_stock_on_hand_on_rqld.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-class AddRemainingQtyInRqld extends Migration {
+class AddRemainingStockOnHandOnRqld extends Migration {
 
     /**
      * Run the migrations.
@@ -17,8 +17,8 @@ class AddRemainingQtyInRqld extends Migration {
 
             Schema::table('tblINV_RQLDetail', function ($table) {
 
-                if (!Schema::hasColumn('tblINV_RQLDetail', 'RQLD_RemainingQty')) {
-                    $table->decimal('RQLD_RemainingQty', 12, 4)->default(0);
+                if (!Schema::hasColumn('tblINV_RQLDetail', 'RQLD_RemainingStockOnHand')) {
+                    $table->decimal('RQLD_RemainingStockOnHand', 12, 4)->default(0);
                 }
             });
 
@@ -40,8 +40,8 @@ class AddRemainingQtyInRqld extends Migration {
 
             Schema::table('tblINV_RQLDetail', function ($table) {
 
-                if (Schema::hasColumn('tblINV_RQLDetail', 'RQLD_RemainingQty')) {
-                    $table->dropColumn('RQLD_RemainingQty');
+                if (Schema::hasColumn('tblINV_RQLDetail', 'RQLD_RemainingStockOnHand')) {
+                    $table->dropColumn('RQLD_RemainingStockOnHand');
                 }
             });
 
