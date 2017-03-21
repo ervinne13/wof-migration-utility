03ce7aed7106876b27abbb507fb494fa5b3e4398
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-03-21 13:55:49 +0800

    added migrations

---
+++ database/migrations/2017_03_21_054649_add_is_password_reset_field_in_user.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsPasswordResetFieldInUser extends Migration
+{
+      /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblCOM_User', function ($table) {
+
+                if (!Schema::hasColumn('tblCOM_User', 'U_IsPasswordReset')) {
+                    $table->boolean('U_IsPasswordReset')->default(false);
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblCOM_User', function ($table) {
+
+                if (Schema::hasColumn('tblCOM_User', 'U_IsPasswordReset')) {
+                    $table->dropColumn('U_IsPasswordReset');
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+}
