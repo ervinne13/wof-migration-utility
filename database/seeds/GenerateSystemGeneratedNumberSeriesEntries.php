f357cc91c58f90a8f21bdb91a7e751be06a5f881
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-03-22 09:17:07 +0800

    completed generation of number series

--- database/seeds/GenerateSystemGeneratedNumberSeriesEntries.php
+++ database/seeds/GenerateSystemGeneratedNumberSeriesEntries.php
@@ -3,6 +3,9 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * php artisan db:seed --class=GenerateSystemGeneratedNumberSeriesEntries
+ */
 class GenerateSystemGeneratedNumberSeriesEntries extends Seeder {
 
     /**
@@ -19,9 +22,19 @@ class GenerateSystemGeneratedNumberSeriesEntries extends Seeder {
         $lastUsedNumbers = DB::select(DB::raw($getLastUsedNumbersRawQuery));
 
         $SGNSEntries = [];
-        foreach() {
-            
+        foreach ($lastUsedNumbers AS $lastUsedNumber) {
+
+            $id = "{$lastUsedNumber->IM_FK_ItemType_id}-{$lastUsedNumber->IM_FK_Category_id}-{$lastUsedNumber->IM_FK_SubCategory_id}";
+
+            array_push($SGNSEntries, [
+                "SGNS_Id"         => $id,
+                "SGNS_StartNo"    => 0,
+                "SGNS_EndingNo"   => 999999,
+                "SGNS_LastNoUsed" => 0,
+            ]);
         }
+
+        DB::table('tblCOM_SystemGeneratedNoSeries')->insert($SGNSEntries);
     }
 
 }
