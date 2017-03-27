8d8d4f7715abec66e65fd85ef09a5982a5a69c51
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-03-27 16:05:25 +0800

    added migrations

---
+++ database/migrations/2017_03_27_071751_add_ref_to_type_to_rq.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRefToTypeToRq extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblINV_RQDetail', function ($table) {
+
+                if (!Schema::hasColumn('tblINV_RQDetail', 'RQD_RefToType')) {
+                    $table->string('RQD_RefToType', 32)->nullable();
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblINV_RQDetail', function ($table) {
+
+                if (Schema::hasColumn('tblINV_RQDetail', 'RQD_RefToType')) {
+                    $table->dropColumn('RQD_RefToType');
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+}
