6aaf278ded5a7abe4786b8686d8ab4be4b0b74f6
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-03-27 08:43:57 +0800

    added migrations

---
+++ database/migrations/2017_03_25_080930_add_transfer_from_sub_location_in_TOL.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTransferFromSubLocationInTOL extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblCOM_TODetail', function ($table) {
+
+                if (!Schema::hasColumn('tblCOM_TODetail', 'TOD_TransferFromSubLocation')) {
+                    $table->string('TOD_TransferFromSubLocation', 32)->nullable();
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblCOM_TODetail', function ($table) {
+
+                if (Schema::hasColumn('tblCOM_TODetail', 'TOD_TransferFromSubLocation')) {
+                    $table->dropColumn('TOD_TransferFromSubLocation');
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+}
