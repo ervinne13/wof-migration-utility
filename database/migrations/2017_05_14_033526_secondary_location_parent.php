7d89098b45e6cdc27ba63aad6d53e78ca78bee50
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-05-14 11:39:52 +0800

    added migrations

---
+++ database/migrations/2017_05_14_033526_secondary_location_parent.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SecondaryLocationParent extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblINV_Location', function ($table) {
+
+                if (!Schema::hasColumn('tblINV_Location', 'LOC_SecondaryRootParent_id')) {
+                    $table->json('LOC_SecondaryRootParent_id')->nullable();
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblINV_Location', function ($table) {
+
+                if (Schema::hasColumn('tblINV_Location', 'LOC_SecondaryRootParent_id')) {
+                    $table->dropColumn('LOC_SecondaryRootParent_id');
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+}
