dfbcfb771c9c43f00af51662978e290ed6046636
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-05-20 17:16:25 +0800

    added  if (!Schema::hasColumn('tblINV_Dispensed', 'DI_FinalizedTicketWeekNo')) {

--- database/migrations/2017_05_20_091342_add_finalizepdweekno.php
+++ database/migrations/2017_05_20_091342_add_finalizepdweekno.php
@@ -4,16 +4,33 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddFinalizepdweekno extends Migration
-{
+class AddFinalizepdweekno extends Migration {
+
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        //
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblINV_Dispensed', function ($table) {
+
+                if (!Schema::hasColumn('tblINV_Dispensed', 'DI_FinalizedPDWeekNo')) {
+                    $table->string('DI_FinalizedPDWeekNo', 20)->nullable();
+                }
+
+                if (!Schema::hasColumn('tblINV_Dispensed', 'DI_FinalizedTicketWeekNo')) {
+                    $table->string('DI_FinalizedTicketWeekNo', 20)->nullable();
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
     }
 
     /**
@@ -21,8 +38,26 @@ class AddFinalizepdweekno extends Migration
      *
      * @return void
      */
-    public function down()
-    {
-        //
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblINV_Dispensed', function ($table) {
+
+                if (Schema::hasColumn('tblINV_Dispensed', 'DI_FinalizedPDWeekNo')) {
+                    $table->dropColumn('DI_FinalizedPDWeekNo');
+                }
+
+                if (!Schema::hasColumn('tblINV_Dispensed', 'DI_FinalizedTicketWeekNo')) {
+                    $table->string('DI_FinalizedTicketWeekNo', 20)->nullable();
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
     }
+
 }
