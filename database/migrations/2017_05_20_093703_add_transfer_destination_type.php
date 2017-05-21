c3844db16bc01f4aeed808c9b164c21aa3abe098
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-05-21 10:28:47 +0800

    added migrations

---
+++ database/migrations/2017_05_20_093703_add_transfer_destination_type.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddTransferDestinationType extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblCOM_TOL', function ($table) {
+
+                if (!Schema::hasColumn('tblCOM_TOL', 'TOL_TransferToType')) {
+                    $table->string('TOL_TransferToType', 30)->nullable();
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblCOM_TOL', function ($table) {
+
+                if (Schema::hasColumn('tblCOM_TOL', 'TOL_TransferToType')) {
+                    $table->dropColumn('TOL_TransferToType');
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+}
