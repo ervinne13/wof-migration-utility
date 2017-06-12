437c938363c1e5a7180d86931fd9bc5b1b93d816
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-06-13 07:04:37 +0800

    added seeders

---
+++ database/seeds/AddRFVLiquidateAccess.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Module\ModuleFunction;
+use App\Models\UserAccess;
+use Illuminate\Database\Seeder;
+
+/**
+ * php artisan db:seed --class=AddRFVLiquidateAccess
+ */
+class AddRFVLiquidateAccess extends Seeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        ModuleFunction::$strictAuditGeneration = false;
+
+        $addApprovalSetupAccess = UserAccess::findOrNew(931);
+
+        $addApprovalSetupAccess->UA_FK_Module_id = 283;
+        $addApprovalSetupAccess->UA_AccessName   = 'Add';
+        $addApprovalSetupAccess->UA_Trigger      = 'financial-management/revolving-fund-voucher/liquidate';
+        $addApprovalSetupAccess->UA_Icon         = "glyphicon-book";
+        $addApprovalSetupAccess->UA_Access_id    = 931;
+        $addApprovalSetupAccess->UA_Inside       = 1;
+        $addApprovalSetupAccess->UA_Inline       = 1;
+        $addApprovalSetupAccess->UA_Get          = 1;
+
+        $addApprovalSetupAccess->save();
+    }
+
+}
