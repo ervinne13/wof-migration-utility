9c2a625a584c7ba980083699e48d9ba0e281066d
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-06-13 08:57:54 +0800

    added posting in liquidate access

--- database/seeds/AddRFVLiquidateAccess.php
+++ database/seeds/AddRFVLiquidateAccess.php
@@ -17,18 +17,30 @@ class AddRFVLiquidateAccess extends Seeder {
     public function run() {
         ModuleFunction::$strictAuditGeneration = false;
 
-        $addApprovalSetupAccess = UserAccess::findOrNew(931);
+        $liquidateAccess = UserAccess::findOrNew(931);
 
-        $addApprovalSetupAccess->UA_FK_Module_id = 283;
-        $addApprovalSetupAccess->UA_AccessName   = 'Liquidate';
-        $addApprovalSetupAccess->UA_Trigger      = 'financial-management/revolving-fund-voucher/liquidate';
-        $addApprovalSetupAccess->UA_Icon         = "glyphicon-book";
-        $addApprovalSetupAccess->UA_Access_id    = 931;
-        $addApprovalSetupAccess->UA_Outside      = 1;
-        $addApprovalSetupAccess->UA_Inline       = 1;
-        $addApprovalSetupAccess->UA_Get          = 1;
+        $liquidateAccess->UA_FK_Module_id = 283;
+        $liquidateAccess->UA_AccessName   = 'Liquidate';
+        $liquidateAccess->UA_Trigger      = 'financial-management/revolving-fund-voucher/liquidate';
+        $liquidateAccess->UA_Icon         = "glyphicon-book";
+        $liquidateAccess->UA_Access_id    = 931;
+        $liquidateAccess->UA_Outside      = 1;
+        $liquidateAccess->UA_Inline       = 1;
+        $liquidateAccess->UA_Get          = 1;
 
-        $addApprovalSetupAccess->save();
+        $liquidateAccess->save();
+
+        $postFuntion = ModuleFunction::findOrNew(226);
+
+        $postFuntion->F_Function_id  = 226;
+        $postFuntion->F_FunctionName = 'Post';
+        $postFuntion->F_FK_Module_id = 283;
+        $postFuntion->F_Order_id     = 10;
+        $postFuntion->F_Trigger      = 'post';
+        $postFuntion->F_Inside       = 1;
+        $postFuntion->F_Outside      = 0;
+
+        $postFuntion->save();
     }
 
 }
