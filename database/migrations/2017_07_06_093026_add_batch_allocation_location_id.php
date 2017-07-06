8c3a31eb4744aec36b04ac67c807c2f72f75a8f8
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-07-06 20:45:11 +0800

    added migrations

---
+++ database/migrations/2017_07_06_093026_add_batch_allocation_location_id.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBatchAllocationLocationId extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblINV_BatchAllocationSubDetails', function ($table) {
+                if (!Schema::hasColumn('tblINV_BatchAllocationSubDetails', 'BASD_LocationCode')) {
+                    $table->string('BASD_LocationCode', 30)->nullable();
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblINV_BatchAllocationSubDetails', function ($table) {
+                if (Schema::hasColumn('tblINV_BatchAllocationSubDetails', 'BASD_LocationCode')) {
+                    $table->dropColumn('BASD_LocationCode');
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+}
