7c9dbabdf8c5d33c4f61b0e68408fbb111138a1e
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-07-24 16:49:53 +0800

    Add location to source of fund

---
+++ database/migrations/2017_07_24_082239_add_sof_number_series.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSofNumberSeries extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblCOM_SourceOfFund', function ($table) {
+                if (!Schema::hasColumn('tblCOM_SourceOfFund', 'SOF_NSId')) {
+                    $table->string('SOF_NSId', 30);
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblCOM_SourceOfFund', function ($table) {
+                if (Schema::hasColumn('tblCOM_SourceOfFund', 'SOF_NSId')) {
+                    $table->dropColumn('SOF_NSId');
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+}
---
+++ database/migrations/2017_07_24_084726_add_sof_location.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSofLocation extends Migration { /**
+ * Run the migrations.
+ *
+ * @return void
+ */
+
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblCOM_SourceOfFund', function ($table) {
+                if (!Schema::hasColumn('tblCOM_SourceOfFund', 'SOF_Location')) {
+                    $table->string('SOF_Location', 30);
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblCOM_SourceOfFund', function ($table) {
+                if (Schema::hasColumn('tblCOM_SourceOfFund', 'SOF_Location')) {
+                    $table->dropColumn('SOF_Location');
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+}
