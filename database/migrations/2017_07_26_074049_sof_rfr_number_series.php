66d91737d2f1d720586c7c99e7c4ae7b72221915
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-07-29 10:13:57 +0800

    Add seeder

---
+++ database/migrations/2017_07_26_074049_sof_rfr_number_series.php
@@ -0,0 +1,61 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SofRfrNumberSeries extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblCOM_SourceOfFund', function ($table) {
+                if (Schema::hasColumn('tblCOM_SourceOfFund', 'SOF_NSId')) {
+                    $table->renameColumn('SOF_NSId', 'SOF_RFVNSId')->nullable();
+                }
+
+                if (!Schema::hasColumn('tblCOM_SourceOfFund', 'SOF_RFRNSId')) {
+                    $table->string('SOF_RFRNSId', 30)->nullable();
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblCOM_SourceOfFund', function ($table) {
+                if (Schema::hasColumn('tblCOM_SourceOfFund', 'SOF_RFVNSId')) {
+                    $table->renameColumn('SOF_RFVNSId', 'SOF_NSId');
+                }
+
+                if (!Schema::hasColumn('tblCOM_SourceOfFund', 'SOF_RFRNSId')) {
+                    $table->dropColumn('SOF_RFRNSId');
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+}
---
+++ database/seeds/AddApprovalAndModuleApprovalDeletion.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\Module\ModuleFunction;
+use App\Models\UserAccess;
+use Illuminate\Database\Seeder;
+
+class AddApprovalAndModuleApprovalDeletion extends Seeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        ModuleFunction::$strictAuditGeneration = false;
+
+        $approvalDeleteAccess = UserAccess::findOrNew(959);
+
+        $approvalDeleteAccess->UA_FK_Module_id = 248;
+        $approvalDeleteAccess->UA_AccessName   = 'Delete';
+        $approvalDeleteAccess->UA_Trigger      = 'l-del';
+        $approvalDeleteAccess->UA_Icon         = "glyphicon-remove";
+        $approvalDeleteAccess->UA_Access_id    = 959;
+        $approvalDeleteAccess->UA_Outside      = 1;
+        $approvalDeleteAccess->UA_Inline       = 1;
+        $approvalDeleteAccess->UA_Get          = 0;
+
+        $approvalDeleteAccess->save();
+
+        $moduleApprovalDeleteAccess = UserAccess::findOrNew(960);
+
+        $moduleApprovalDeleteAccess->UA_FK_Module_id = 322;
+        $moduleApprovalDeleteAccess->UA_AccessName   = 'Delete';
+        $moduleApprovalDeleteAccess->UA_Trigger      = 'l-del';
+        $moduleApprovalDeleteAccess->UA_Icon         = "glyphicon-remove";
+        $moduleApprovalDeleteAccess->UA_Access_id    = 960;
+        $moduleApprovalDeleteAccess->UA_Outside      = 1;
+        $moduleApprovalDeleteAccess->UA_Inline       = 1;
+        $moduleApprovalDeleteAccess->UA_Get          = 0;
+
+        $moduleApprovalDeleteAccess->save();
+    }
+
+}
