a451a2a56dc468c0816cd97b4c05c9f1316fa259
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-08-10 17:41:58 +0800

    added migrations

---
+++ database/migrations/2017_08_10_094051_add_converted_flag_to_IRJ.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddConvertedFlagToIRJ extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblINV_IRJ', function ($table) {
+                if (!Schema::hasColumn('tblINV_IRJ', 'IRJ_Converted')) {
+                    $table->boolean('IRJ_Converted')->default(false);
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblINV_IRJ', function ($table) {
+                if (!Schema::hasColumn('tblINV_IRJ', 'IRJ_Converted')) {
+                    $table->dropColumn('IRJ_Converted');
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+}
