73fc8d119bd9b9d786e634c6f978d81e33192756
Ervinne Sodusta <ervinnesodusta13@yahoo.com.ph>
2017-08-16 13:41:43 +0800

    Add migrations

---
+++ database/migrations/2017_08_16_034155_add_RFV_liquidation_RR_tables.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRFVLiquidationRRTables extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        $query = 'CREATE TABLE public."tblACC_RFVRRLiquidationDetail"
+            (
+              "RFVRRD_RFV_DocNo" character varying(30) NOT NULL,
+              "RFVRRD_RR_DocNo" character varying(30) NOT NULL,
+              "RFVRRD_LineNo" bigserial,
+              "RFVRRD_ItemType" character varying(30),
+              "RFVRRD_ItemNo" character varying(30),
+              "RFVRRD_ItemDescription" character varying(250),
+              "RFVRRD_Location" character varying(30),
+              "RFVRRD_QtyReceived" numeric(12,4),
+              "RFVRRD_UnitPrice" numeric(12,4),
+              "RFVRRD_Total" numeric(12,4),
+              "RFVRRD_Currency" character varying(20),
+              "RFVRRD_Comment" text,
+              "RFVRRD_RefFrom" character varying(30),
+              "RFVRRD_RefFromLineNo" integer,
+              "RFVRRD_VAT" character varying(30),
+              "RFVRRD_WHT" character varying(30),
+              "RFVRRD_RefTo" character varying(30),
+              "RFVRRD_RefToLineNo" character varying(30),
+              "RFVRRD_TotalLCY" numeric(30,12),
+              "CreatedBy" character varying(20),
+              "DateCreated" timestamp without time zone,
+              "ModifiedBy" character varying(20),
+              "DateModified" timestamp without time zone,
+              "RFVRRD_UOM" bigint,
+              "RFVRRD_VATAmount" numeric(12,4) NOT NULL DEFAULT \'0\'::numeric,
+              "RFVRRD_Date" date,
+              "RFVRRD_ExternalDocNo" character varying(100),
+              "RFVRRD_TIN" character varying(100),
+              "RFVRRD_NetofVAT" numeric(12,4) NOT NULL DEFAULT \'0\'::numeric,
+              "RFVRRD_SupplierID" character varying(30),
+              "RFVRRD_RR_RefFrom" character varying(30),
+              CONSTRAINT "RFVRRD_RR_DocNo" PRIMARY KEY ("RFVRRD_RR_DocNo", "RFVRRD_LineNo")
+            )
+            WITH (
+              OIDS=FALSE
+            );';
+        DB::statement($query);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('tblACC_RFVRRLiquidationDetail');
+    }
+
+}
---
+++ database/migrations/2017_08_16_034208_add_RFV_liquidation_RR_fields.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRFVLiquidationRRFields extends Migration { /**
+ * Run the migrations.
+ *
+ * @return void
+ */
+
+    public function up() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblACC_RFV', function ($table) {
+                if (!Schema::hasColumn('tblACC_RFV', 'RFV_RRBased')) {
+                    $table->boolean('RFV_RRBased')->default(false);
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        try {
+            DB::beginTransaction();
+
+            Schema::table('tblACC_RFV', function ($table) {
+                if (!Schema::hasColumn('tblACC_RFV', 'RFV_RRBased')) {
+                    $table->dropColumn('RFV_RRBased');
+                }
+            });
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+}
